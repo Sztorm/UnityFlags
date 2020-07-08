f072b5d9784eb6113137a00a434592eebe594b45
Sztorm <marcinwicher99@gmail.com>
2020-07-08 02:55:26 +0200

    Added all files and set up repository to Unity environment.

---
+++ Scripts/FlagFieldsAttribute.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Sztorm.Unity.Flags
+{
+    public class FlagFieldsAttribute : PropertyAttribute
+    {
+        public readonly string[] Names;
+        public readonly int Count;
+
+        public FlagFieldsAttribute(
+            string flagName0 = null,
+            string flagName1 = null,
+            string flagName2 = null,
+            string flagName3 = null,
+            string flagName4 = null,
+            string flagName5 = null,
+            string flagName6 = null,
+            string flagName7 = null)
+        {
+            Names = new string[8];
+            Names[0] = flagName0;
+            Names[1] = flagName1;
+            Names[2] = flagName2;
+            Names[3] = flagName3;
+            Names[4] = flagName4;
+            Names[5] = flagName5;
+            Names[6] = flagName6;
+            Names[7] = flagName7;
+
+            int count = 0;
+
+            for (int i = 0; i < Names.Length; i++)
+            {
+                if (Names[i] != null)
+                {
+                    count++;
+                }
+            }
+            Count = count;
+        }
+    }
+}
---
+++ Scripts/FlagFieldsDrawer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+using Sztorm.Extensions.Enum;
+
+namespace Sztorm.Unity.Flags
+{
+    [Flags]
+    internal enum BitFlags8 : int
+    {
+        None = 0,
+        Bit1 = 1,
+        Bit2 = 2,
+        Bit3 = 4,
+        Bit4 = 8,
+        Bit5 = 16,
+        Bit6 = 32,
+        Bit7 = 64,
+        Bit8 = -128,
+    }
+
+    [CustomPropertyDrawer(typeof(FlagFieldsAttribute))]
+    internal class FlagsFieldsDrawer : PropertyDrawer
+    {
+        private const float ToggleControlHeight = 18;
+        private const float InspectorBottomPadding = 2;
+
+        private bool isCached = false;
+        private bool underlyingEnumTypeIsSByte;
+        private float toggleHeight;
+        private FlagFieldsAttribute flagFields;
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            if (!isCached)
+            {
+                flagFields = attribute as FlagFieldsAttribute;
+            }
+            return ToggleControlHeight * flagFields.Count - InspectorBottomPadding;
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (!isCached)
+            {
+                toggleHeight = GUI.skin.toggle.padding.vertical + GUI.skin.toggle.border.vertical;
+                flagFields = attribute as FlagFieldsAttribute;
+                Type underlyingEnumType = Enum.GetUnderlyingType(fieldInfo.FieldType);
+                underlyingEnumTypeIsSByte = underlyingEnumType.Name == "SByte";
+                isCached = true;
+            }
+            BitFlags8 flags = (BitFlags8)(property.intValue);
+            Vector2 toggleRectSize = new Vector2(position.size.x, toggleHeight);
+
+            EditorGUI.BeginProperty(position, label, property);
+
+            for (int i = 0, propIndex = 0, length = flagFields.Names.Length; i < length; i++)
+            {
+                if (flagFields.Names[i] != null)
+                {
+                    BitFlags8 flag = (BitFlags8)(1 << i);
+
+                    bool isChecked = EditorGUI.Toggle(
+                        new Rect(
+                            new Vector2(position.x, position.y + ToggleControlHeight * propIndex),
+                            toggleRectSize),
+                        flagFields.Names[i],
+                        flags.HasAllFlags(flag));
+
+                    flags = flags.WithFlagsSetTo(flag, isChecked);
+                    propIndex++;
+                }
+            }
+            if (underlyingEnumTypeIsSByte)
+            {
+                property.intValue = (sbyte)flags;
+            }
+            else
+            {
+                property.intValue = (byte)flags;
+            }
+            EditorGUI.EndProperty();
+        }
+    }
+}
